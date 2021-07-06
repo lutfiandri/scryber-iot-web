015068a1bd73317a971bc9bdcf6737aca67df6f7
Lutfi Andriyanto <lutfiandri232@gmail.com>
2021-07-06 15:10:05 +0700

    feat: setup chakra ui

---
+++ styles/chakra.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    dark: '#293800',
+    light: '#B3C760',
+  },
+};
+
+const theme = extendTheme({ colors });
+export default theme;
